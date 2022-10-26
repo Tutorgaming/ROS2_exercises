677c287bb6ff6c954646814af84f61453c28c0e8
tutor-obo <theppasith.n@obodroid.com>
2022-10-26 15:02:37 +0700

    finish Assignment

--- turtlesim_control/launch/full_example.launch.py
+++ turtlesim_control/launch/full_example.launch.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import  ExecuteProcess, RegisterEventHandler, LogInfo, EmitEvent, OpaqueFunction
@@ -13,20 +14,29 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+# Assignment 9 - Read YAML
+from ament_index_python.packages import get_package_share_directory
+
 def generate_launch_description():
     follower_gain = LaunchConfiguration('gain')
     follower_gain_launch_arg = DeclareLaunchArgument('gain',default_value='5.0')
     follower_speed = LaunchConfiguration('speed')
     follower_speed_launch_arg = DeclareLaunchArgument('speed',default_value='2.0')
 
+    # Assignment 9 - Read YAML for LEADER
+    turtlesim_control_pkg_path = get_package_share_directory("turtlesim_control")
+    param_path = os.path.join(turtlesim_control_pkg_path, "config", "leader_config.yaml")
     leader = Node(
         package='turtlesim_control',
         executable='via_point_follower.py',
         namespace="turtle1",
-        parameters=[{
-            "gain" : 5.0,
-            "speed" : 2.0
-        },]
+        parameters=[
+            param_path # Read YAML instead
+            # {
+            #     "gain" : 5.0,
+            #     "speed" : 2.0,
+            # }
+        ,]
     )
 
     scheduler = Node(
