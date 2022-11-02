65a82919a7271a84ba334128fd0a8e6b7b2333ef
tutor-obo <theppasith.n@obodroid.com>
2022-11-02 12:08:36 +0700

    Finished Exercise 2 Day 4

--- manipulator/dummy_gazebo/launch/spawn.launch.py
+++ manipulator/dummy_gazebo/launch/spawn.launch.py
@@ -1,15 +1,15 @@
 #!usr/bin/python3
 
 """
-This program is free software: you can redistribute it and/or modify it 
-under the terms of the GNU General Public License as published by the Free Software Foundation, 
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free Software Foundation,
 either version 3 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with this program. 
+You should have received a copy of the GNU General Public License along with this program.
 If not, see <https://www.gnu.org/licenses/>.
 
 created by Thanacha Choopojcharoen at CoXsys Robotics (2022)
@@ -33,6 +33,7 @@ class ShareFile():
         self.folder = folder
         self.file = file
         self.path = os.path.join(self.package_path,folder,file)
+
 def launch_action_gazebo():
     gazebo_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
@@ -53,24 +54,42 @@ def launch_action_gazebo():
         ])
     )
     return gazebo_server,gazebo_client
-def launch_action_robot_spawner(dh_parameters,robot_description,position):
+
+def launch_action_robot_spawner(dh_parameters, robot_description, position, robot_name=""):
     # robot_state_publisher
-    DH2Transform(dh_parameters.package_name,dh_parameters.folder,dh_parameters.file) 
+    DH2Transform(dh_parameters.package_name,dh_parameters.folder,dh_parameters.file)
     parameters = []
     robot_desc_xml = xacro.process_file(robot_description.path).toxml()
-    parameters.append({'robot_description':robot_desc_xml})
+    parameters.append({'robot_description':robot_desc_xml, "frame_prefix": robot_name + "/"})
     parameters.append({'use_sim_time': True})
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='both',
-        parameters=parameters
-    )    
-    
-    
+        parameters=parameters,
+        namespace=robot_name,
+    )
+
+    # Create Gazebo Robot Spawner
+    robot_spawner = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        output="screen",
+        arguments=[
+            "-topic", "{}/robot_description".format(robot_name),
+            "-entity", str(robot_name),
+            '-x', str(position[0]) ,
+            '-y', str(position[1]) ,
+            '-z', str(position[2]) ,
+            '-R', '0.0',
+            '-P', '0.0',
+            '-Y', '0.0',
+        ]
+    )
     actions = []
     actions.append(robot_state_publisher)
-    
+    actions.append(robot_spawner)
+
     return actions
 def recursive_yaml(value):
     if isinstance(value,dict):
@@ -90,18 +109,32 @@ def generate_controller_config(controller,namespace):
 def generate_launch_description():
     dh_parameters = ShareFile('dummy_description','config','DH_parameters.yaml')
     robot_description = ShareFile('dummy_gazebo','robot','dummy.xacro')
-    
+
     gazebo_server,gazebo_client = launch_action_gazebo()
-    actions = launch_action_robot_spawner(
-        dh_parameters,
-        robot_description,
-        [0.0,0.0,0.0]
-    )
+    # Create Single robot
+    # actions = launch_action_robot_spawner(
+    #     dh_parameters,
+    #     robot_description,
+    #     [0.0,0.0,0.0],
+    #     robot_name=robot_name
+    # )
+    # Create Many Robot
+    actions = []
+    for i in range(0,5):
+        robot_name = "dummy_{}".format(i)
+        actions+= launch_action_robot_spawner(
+                dh_parameters,
+                robot_description,
+                [i*2.0,0.0,0.0],
+                robot_name=robot_name
+        )
+
     launch_description = LaunchDescription()
     launch_description.add_action(gazebo_server)
     launch_description.add_action(gazebo_client)
+
     for action in actions:
         launch_description.add_action(action)
-    
+
     return launch_description
-    
+
